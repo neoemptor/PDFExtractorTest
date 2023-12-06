97f865baba5812b7a4c164b0b58c0bcfc8054d00
Darren Bailey <darrensb@iinet.net.au>
2023-12-06 14:48:57 +0800

    Add project files.

---
+++ PDFExtractorTest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PDFExtractorTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rtbStats = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(355, 178);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 50);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // rtbStats
+            // 
+            this.rtbStats.Location = new System.Drawing.Point(24, 247);
+            this.rtbStats.Name = "rtbStats";
+            this.rtbStats.Size = new System.Drawing.Size(764, 183);
+            this.rtbStats.TabIndex = 1;
+            this.rtbStats.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbStats);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rtbStats;
+    }
+}
+
---
+++ PDFExtractorTest/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PDFExtractorTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string pdfPath = "C:\\temp_nci\\Credit_Application.pdf";
+            string exclusionFilePath = ".\\exclusion_file.txt"; // Path to the file containing phrases to exclude from processing
+            string fieldsListPath = ".\\fields_list.json"; // Path to the file containing field names
+
+            string[] exclusionPhrases = File.ReadAllLines(exclusionFilePath);
+            HashSet<string> exclusions = new HashSet<string>(exclusionPhrases);
+
+            // Load field names from JSON file
+            string jsonText = File.ReadAllText(fieldsListPath);
+            var fieldCategories = JsonConvert.DeserializeObject<Dictionary<string, JToken>>(jsonText);
+
+            // Combine all fields into a single list
+            List<string> fieldNames = new List<string>();
+
+            foreach (var category in fieldCategories)
+            {
+                AddFieldNamesFromToken(category.Value, fieldNames);
+            }
+
+            PdfReader reader = new PdfReader(pdfPath);
+
+            StringBuilder text = new StringBuilder();
+
+            // Step 2: Process each page
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+            {
+                string pageText = PdfTextExtractor.GetTextFromPage(reader, i);
+                string[] lines = pageText.Split('\n');
+
+                foreach (string line in lines)
+                {
+                    bool excludeLine = false;
+
+                    foreach (string phrase in exclusions)
+                    {
+                        if (line.Contains(phrase))
+                        {
+                            excludeLine = true;
+                            break;
+                        }
+                    }
+
+                    if (!excludeLine && line.Trim().Length > 0)
+                    {
+                        text.AppendLine(line); // Append the line if it does not contain any exclusion phrases
+                        rtbStats.AppendText(line + '\n');
+                    }
+                }
+
+            }
+
+
+
+
+            reader.Close();
+
+            // Step 3: Extract field values
+            string allText = text.ToString();
+
+            // Merge specific split lines
+            allText = MergeSpecificLines(allText);
+            Dictionary<string, string> fieldValues = new Dictionary<string, string>();
+            for (int i = 0; i < fieldNames.Count; i++)
+            {
+                string startField = fieldNames[i];
+                string endField = i < fieldNames.Count - 1 ? fieldNames[i + 1] : null;
+                int startIndex = allText.IndexOf(startField) + startField.Length;
+                int endIndex = endField != null ? allText.IndexOf(endField, startIndex) : allText.Length;
+
+                string fieldValue = startIndex < endIndex ? allText.Substring(startIndex, endIndex - startIndex).Trim() : "";
+                fieldValues[startField] = fieldValue;
+            }
+
+            // Output the field values
+            foreach (var fieldValue in fieldValues)
+            {
+                Console.WriteLine($"{fieldValue.Key}: {fieldValue.Value}");
+            }
+
+        }
+        private static IEnumerable<string> FlattenFieldList(JToken token)
+        {
+            if (token.Type == JTokenType.Array)
+            {
+                foreach (var item in token.Children())
+                {
+                    if (item.Type == JTokenType.String)
+                    {
+                        yield return item.ToString();
+                    }
+                    else if (item.Type == JTokenType.Array)
+                    {
+                        foreach (var nestedItem in item.Children())
+                        {
+                            if (nestedItem.Type == JTokenType.String)
+                            {
+                                yield return nestedItem.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private static void AddFieldNamesFromToken(JToken token, List<string> fieldNames)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Array:
+                    foreach (var item in token.Children())
+                    {
+                        AddFieldNamesFromToken(item, fieldNames);
+                    }
+                    break;
+                case JTokenType.String:
+                    fieldNames.Add(token.ToString());
+                    break;
+                    // Add cases for other JToken types if necessary
+            }
+        }
+
+        private static string MergeSpecificLines(string text)
+        {
+            // Define the split line and how it should be merged
+            string splitLineStart = "How long have you been trading in current";
+            string splitLineEnd = "location?";
+
+            // Replace the newline between the split lines with a space
+            return text.Replace(splitLineStart + "\n" + splitLineEnd, splitLineStart + " " + splitLineEnd);
+        }
+
+    }
+}
